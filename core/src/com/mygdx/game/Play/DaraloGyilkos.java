3e24030e2039289a6f7c1bb3db32261b33296bef
Vince <egyed.vince1998@gmail.com>
2017-02-03 11:26:26 +0100

    asetesek + eső dolgok osztálya

--- core/src/com/mygdx/game/Play/DaraloGyilkos.java
+++ core/src/com/mygdx/game/Play/DaraloGyilkos.java
@@ -8,8 +8,21 @@ import com.mygdx.game.MyBaseClasses.OneSpriteStaticActor;
  */
 public class DaraloGyilkos extends OneSpriteStaticActor {
 
+    private int speed = 1;
+
     public DaraloGyilkos(Texture texture) {
         super(texture);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        setPosition(getX(), getY()-2*speed);
+
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 
 
--- core/src/com/mygdx/game/Play/NemHusok.java
+++ core/src/com/mygdx/game/Play/NemHusok.java
@@ -7,7 +7,21 @@ import com.mygdx.game.MyBaseClasses.OneSpriteStaticActor;
  * Created by tanulo on 2017. 02. 03..
  */
 public class NemHusok extends OneSpriteStaticActor {
+
+    private int speed = 1;
+
     public NemHusok(Texture texture) {
         super(texture);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        setPosition(getX(), getY()-2*speed);
+
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 }
