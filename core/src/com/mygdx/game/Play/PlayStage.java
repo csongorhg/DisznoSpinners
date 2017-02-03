07b6eb9a491bc5a5a22e45523ef570b870ec5696
mordes <csongorhegedus2@indamail.hu>
2017-02-03 11:38:23 +0100

    Pálesz

--- core/src/com/mygdx/game/Play/PlayStage.java
+++ core/src/com/mygdx/game/Play/PlayStage.java
@@ -2,20 +2,13 @@ package com.mygdx.game.Play;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Vector;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.GlobalClasses.Assets;
-import com.mygdx.game.MyBaseClasses.MyButton;
 import com.mygdx.game.MyBaseClasses.MyStage;
-import com.mygdx.game.MyBaseClasses.OneSpriteAnimatedActor;
 import com.mygdx.game.MyBaseClasses.OneSpriteStaticActor;
 import com.mygdx.game.MyGdxGame;
-
-import java.util.ArrayList;
 
 /**
  * Created by tuskeb on 2016. 09. 30..
@@ -24,9 +17,11 @@ public class PlayStage extends MyStage {
     private TextButton textButton;
 
     private KolbaszTolto kolbaszTolto;
-    private static ArrayList<OneSpriteStaticActor> potyogoDolgok;
 
     private static int dbPotyogas;
+
+    private static OneSpriteStaticActor palinkaKijelzoActor;
+    public static int palinkaSzint, jelenlegiPalinkaSzint;
 
     public PlayStage(Viewport viewport, Batch batch, MyGdxGame game) {
         super(viewport, batch, game);
@@ -36,20 +31,52 @@ public class PlayStage extends MyStage {
     public void init() {
         addBackEventStackListener();
 
+
+
+        //pálinka
+        palinkaSzint = 15;
+        palinkaKijelzoActor = new OneSpriteStaticActor(new PalinkaKijelzo().getTexture());
+        addActor(palinkaKijelzoActor);
+        palinkaKijelzoActor.setSize(50,200);
+        palinkaKijelzoActor.setPosition(getViewport().getWorldWidth() - palinkaKijelzoActor.getWidth(),0);
+        jelenlegiPalinkaSzint = palinkaSzint;
+        //pálinka
+
+
+
         kolbaszTolto = new KolbaszTolto();
         addActor(kolbaszTolto);
 
         Husok husok = new Husok(Assets.manager.get(Assets.TEST_TEXTURE));
         addActor(husok);
-        potyogoDolgok.add(husok);
-    }
+
+        palinkaszint();
+
+        }
 
     @Override
     public void act(float delta) {
         super.act(delta);
         pozicionalas();
+
+        //pálinkaszint váltása
+        palinkaSzint--;
+        palinkaszint();
+        //pálinkaszint váltása
+
     }
 
+    private void palinkaszint() {
+        if (palinkaKijelzoActor != null && jelenlegiPalinkaSzint != palinkaSzint) {
+            System.out.println("asd");
+            palinkaKijelzoActor.remove();
+            palinkaKijelzoActor = new OneSpriteStaticActor(new PalinkaKijelzo().getTexture());
+            addActor(palinkaKijelzoActor);
+            palinkaKijelzoActor.setSize(50,200);
+            palinkaKijelzoActor.setPosition(getViewport().getWorldWidth() - palinkaKijelzoActor.getWidth(),0);
+            jelenlegiPalinkaSzint = palinkaSzint;
+            }
+        }
 
     private void pozicionalas(){
         if(kolbaszTolto.getX() >=0 && kolbaszTolto.getX()+kolbaszTolto.getWidth() <= ((ExtendViewport)getViewport()).getMinWorldWidth())
